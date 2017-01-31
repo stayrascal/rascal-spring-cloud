7354f10524ca0809f1f19110ade19b8a3ef1738e
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-31 21:24:24 +0800

    add install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# update apt-get
+apt-get update
+
+apt-get install --assume-yes iputils-ping
+apt-get install --assume-yes vim
+apt-get install --assume-yes git
+apt-get install --assume-yes telnet
+apt-get install --assume-yes net-tools
+apt-get install --assume-yes man
