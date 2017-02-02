6b4e159a0b71620d4857ab1584b667348c990965
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-02 11:11:09 +0800

    update api-admin project

---
+++ api-admin/src/main/java/com/stayrascal/could/admin/Application.java
@@ -0,0 +1,13 @@
+package com.stayrascal.could.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
