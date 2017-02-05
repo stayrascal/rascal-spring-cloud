01f2f3a9105373082a20d6df2411567d88d126fb
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-05 20:32:17 +0800

    add controller of v1 version in users-service

--- users-service/src/main/java/com/stayrascal/api/users/v1/controller/MovieController.java
+++ users-service/src/main/java/com/stayrascal/api/users/v1/controller/MovieController.java
@@ -1,7 +1,21 @@
 package com.stayrascal.api.users.v1.controller;
 
+import com.stayrascal.api.users.v1.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @RequestMapping("/graph")
+    public Map<String, Object> graph(@RequestParam(value = "limit", required = false) Integer limit){
+        return movieService.graph(limit);
+    }
 }
