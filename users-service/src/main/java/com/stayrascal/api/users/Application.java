691e0dcf7f20d51fbc2b283806aa8de1dd041df2
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-05 21:39:41 +0800

    add configuration of v1 version in users-service

--- users-service/src/main/java/com/stayrascal/api/users/Application.java
+++ users-service/src/main/java/com/stayrascal/api/users/Application.java
@@ -1,31 +1,20 @@
 package com.stayrascal.api.users;
 
-import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableHystrix
-@EnableZuulProxy
-@EnableNeo4jRepositories
-public class Application extends Neo4jConfiguration {
-
-    public Application() {
-    }
-
-    @Override
-    public SessionFactory getSessionFactory() {
-        return null;
-    }
+//@EnableDiscoveryClient
+//@EnableHystrix
+//@EnableZuulProxy
+@EnableNeo4jRepositories(basePackages = "com.stayrascal.api.users.v1.repositories")
+@EntityScan(basePackages = "com.stayrascal.api.users.v1.domain")
+public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
+
 }
--- users-service/src/main/java/com/stayrascal/api/users/v1/controller/MovieController.java
+++ users-service/src/main/java/com/stayrascal/api/users/v1/controller/MovieController.java
@@ -15,7 +15,7 @@ public class MovieController {
     private MovieService movieService;
 
     @RequestMapping("/graph")
-    public Map<String, Object> graph(@RequestParam(value = "limit", required = false) Integer limit){
-        return movieService.graph(limit);
+    public Map<String, Object> graph(@RequestParam(value = "limit", required = false) Integer limit) {
+        return movieService.graph(limit == null ? 100 : limit);
     }
 }
