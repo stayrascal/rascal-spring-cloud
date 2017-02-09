631b7ec5c9a12b1069235fe027f363c9b3c2ad1d
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-09 22:26:40 +0800

    add movie repository

--- movie-service/src/main/java/com/stayrascal/api/movie/repositories/MovieRepository.java
+++ movie-service/src/main/java/com/stayrascal/api/movie/repositories/MovieRepository.java
@@ -1,13 +1,13 @@
 package com.stayrascal.api.movie.repositories;
 
-import com.stayrascal.api.movie.model.nodes.Genre;
+import com.stayrascal.api.movie.model.nodes.Movie;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "genre")
-public interface GenreRepository extends PagingAndSortingRepository<Genre, Long> {
-    List<Genre> findByName(@Param("name") String name);
+@RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
+public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
+    List<Movie> findByTitle(@Param("title") String title);
 }
