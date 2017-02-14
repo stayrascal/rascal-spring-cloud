aa9c193e69764683fc83b3100a46ec351f09567d
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-14 21:08:01 +0800

    update global-config project

--- api-gateway/src/main/java/com/stayrascal/cloud/api/gateway/Application.java
+++ api-gateway/src/main/java/com/stayrascal/cloud/api/gateway/Application.java
@@ -1,4 +1,4 @@
-package com.stayrascal.cloud.api;
+package com.stayrascal.cloud.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
