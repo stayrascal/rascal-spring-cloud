97da978fd1163794a00a8318fd58d831f1118dc4
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-14 21:00:30 +0800

    update api-gateway project

--- api-gateway/src/main/java/com/stayrascal/cloud/api/Application.java
+++ api-gateway/src/main/java/com/stayrascal/cloud/api/Application.java
@@ -3,11 +3,13 @@ package com.stayrascal.cloud.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableSidecar
 public class Application {
 
     public static void main(String[] args) {
