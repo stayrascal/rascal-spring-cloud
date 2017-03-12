ed1454b6c8994a6d4284ac71b1f766725f146ed3
Zhiping Wu <zpwu@thoughtworks.com>
2017-03-12 22:44:23 +0800

    update global-config sub project

--- global-config/src/main/java/com/stayrascal/cloud/api/config/Application.java
+++ global-config/src/main/java/com/stayrascal/cloud/api/config/Application.java
@@ -3,10 +3,11 @@ package com.stayrascal.cloud.api.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableConfigServer
+@EnableConfigServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
